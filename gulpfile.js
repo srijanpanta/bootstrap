e4ea93dc0b31bb6faaf5cf4b4207df392cf1e747
srijanpanta <51430521+srijanpanta@users.noreply.github.com>
2020-02-29 18:40:55 +0545

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+
+var gulp = require('gulp');
+
+var browserSync = require('browser-sync').create();
+
+var sass = require('gulp-sass');
+var connect = require("connect");
+
+var app = connect.createServer().use(connect.static(__dirname + '/public'));
+
+app.listen(8180);
+
+// Compile sass into CSS & auto-inject into browsers
+
+gulp.task('sass', function() {
+
+return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+
+.pipe(sass())
+
+.pipe(gulp.dest("src/css"))
+
+.pipe(browserSync.stream());
+
+});
+
+// Move the javascript files into our /src/js folder
+
+gulp.task('js', function() {
+
+return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+
+.pipe(gulp.dest("src/js"))
+
+.pipe(browserSync.stream());
+
+});
+
+// Static Server + watching scss/html files
+
+gulp.task('serve', gulp.series('sass', function() {
+
+browserSync.init({
+
+server: "./src"
+
+});
+
+gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], gulp.series('sass'));
+
+gulp.watch("src/*.html").on('change', browserSync.reload);
+
+}));
+
+gulp.task('default', gulp.parallel('js','serve'));
